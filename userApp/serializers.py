b0215619f783a7b3e31335dfc704d5cdb7c0528c
Ogunkunle Benjamin Timileyin <bentimi02@gmail.com>
2026-03-07 19:13:52 +0100

    update user

--- userApp/serializers.py
+++ userApp/serializers.py
@@ -1,5 +1,22 @@
 from rest_framework import serializers
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 
 class OtpSerializer(serializers.Serializer):
     code = serializers.CharField(required=True, max_length=6)
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        field = '__all__'
+        exclude = ['is_staff', 'is_superuser', 'is_active', 'user_permissions', 'groups']
+
+        extra_kwargs = {
+            'password': {'write_only': True},
+        }
+
+class UpdateUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['first_name', 'last_name', 'phone_number', 'email', 'username']
--- userApp/views.py
+++ userApp/views.py
@@ -4,7 +4,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework import status
-from userApp.serializers import OtpSerializer
+from userApp.serializers import OtpSerializer, UpdateUserSerializer, UserSerializer
 from userApp.models import Otp
 from django.utils import timezone
 from rest_framework_simplejwt.tokens import RefreshToken
@@ -64,25 +64,23 @@ def verify_otp(request):
 
     return response
 
-@api_view(['GET', 'PUT'])
+@api_view(['GET', 'PATCH'])
 @permission_classes([IsAuthenticated])
 @ensure_csrf_cookie
 def get_user_details(request):
 
     if request.method == 'GET':
         user = request.user
-        return Response({
-            "username": user.username,
-            "email": user.email,
-            "role": user.role
-        }, status=status.HTTP_200_OK)
+        serializer = UserSerializer(user)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
-    if request.method == 'PUT':
+    if request.method == 'PATCH':
         user = request.user
 
-        return Response({
-            "message": "User details updated successfully",
-            "username": user.username,
-            "email": user.email,
-            "role": user.role
-        }, status=status.HTTP_200_OK)
+        serializer = UpdateUserSerializer(user, data=request.data, partial=True)
+        if not serializer.is_valid():
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+        serializer.save()
+
+        return Response(serializer.data, status=status.HTTP_200_OK)
